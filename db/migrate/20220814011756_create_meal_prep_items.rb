c73dc9f5b08f1ef913ace45929a33aa62bb33888
Takahiro Masui <takahiro.masui@luxiar.com>
2022-08-28 09:36:16 +0900

    migrationファイルにコメント追加

--- db/migrate/20220814011756_create_meal_prep_items.rb
+++ db/migrate/20220814011756_create_meal_prep_items.rb
@@ -1,10 +1,10 @@
 class CreateMealPrepItems < ActiveRecord::Migration[7.0]
   def change
-    create_table :meal_prep_schedule_items do |t|
-      t.string :name, null: false
-      t.boolean :prepared, null: false, default: false
-      t.integer :meal_type, null: false
-      t.references :meal_prep_schedule, foreign_key: true
+    create_table :meal_prep_schedule_items, comment: '作り置きアイテム' do |t|
+      t.string :name, null: false, comment: '名前'
+      t.boolean :prepared, null: false, default: false, comment: '作成済み'
+      t.integer :meal_type, null: false, comment: '作り置き種類'
+      t.references :meal_prep_schedule, foreign_key: true, comment: '作り置きスケジュールID'
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_14_011756) do
-  create_table "meal_prep_schedule_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.boolean "prepared", default: false, null: false
-    t.integer "meal_type", null: false
-    t.bigint "meal_prep_schedule_id"
+  create_table "meal_prep_schedule_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", comment: "作り置きアイテム", force: :cascade do |t|
+    t.string "name", null: false, comment: "名前"
+    t.boolean "prepared", default: false, null: false, comment: "作成済み"
+    t.integer "meal_type", null: false, comment: "作り置き種類"
+    t.bigint "meal_prep_schedule_id", comment: "作り置きスケジュールID"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["meal_prep_schedule_id"], name: "index_meal_prep_schedule_items_on_meal_prep_schedule_id"
   end
 
-  create_table "meal_prep_schedules", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
+  create_table "meal_prep_schedules", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", comment: "作り置きスケジュール", force: :cascade do |t|
+    t.string "name", null: false, comment: "名前"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
