55246f12fce55d65be61a5fddd24ff7b3f0f7547
Takahiro Masui <takahiro.masui@luxiar.com>
2022-08-28 14:05:51 +0900

    消化率を追加

--- app/models/meal_prep_schedule/item.rb
+++ app/models/meal_prep_schedule/item.rb
@@ -1,6 +1,14 @@
 class MealPrepSchedule::Item < ApplicationRecord
   belongs_to :schedule, class_name: 'MealPrepSchedule', foreign_key: "meal_prep_schedule_id"
   validates :name, presence: true
+  validate :consumption_rate_can_not_update_in_unprepared_item
+  validates :consumption_rate, numericality: {greater_than_or_equal_to: 0,less_than_or_equal_to: 100}
 
   enum :meal_type, { main: 0, side: 1 }
+
+  def consumption_rate_can_not_update_in_unprepared_item
+    if prepared.blank? && consumption_rate != 0
+      errors.add(:consumption_rate, 'cannot be update when item is not prepared')
+    end
+  end
 end
