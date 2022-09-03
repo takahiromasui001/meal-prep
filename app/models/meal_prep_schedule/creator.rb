cb7c3991b80874e38cfa0a9d496213eadb832ead
Takahiro Masui <takahiro.masui@luxiar.com>
2022-09-03 12:58:25 +0900

    #14 spec修正

--- app/models/meal_prep_schedule/creator.rb
+++ app/models/meal_prep_schedule/creator.rb
@@ -1,10 +1,21 @@
 class MealPrepSchedule::Creator
-  def create_schedule!(meal_prep_schedule_params, initial_count_params)
+  def create_schedule!(meal_prep_schedule_params, initial_count_params, base_item_schedule_id = nil)
     meal_prep_schedule = MealPrepSchedule.new(meal_prep_schedule_params)
 
     ActiveRecord::Base.transaction do
-      result = meal_prep_schedule.save!
+      new_schedule = meal_prep_schedule.save!
 
+      # 引き継ぎアイテムの作成
+      # new_schedule.hand_over_meal_prep_schedule_items!(base_schedule_id)
+      # Handover.hand_over_items!(new_schedule, base_schedule_id)
+
+      base_schedule = MealPrepSchedule.find(1)
+      base_schedule.items.prepared.map(&:dup).each do |item| {
+        item.meal_prep_schedule_id = new_schedule.id
+        item.save!
+      }
+
+      # meal pointから残りの要作成アイテムを計算
       create_initial_item_params(initial_count_params).each do |item_param|
         meal_prep_schedule.items.create!(item_param)
       end
--- spec/models/meal_prep_schedule_creator_spec.rb
+++ spec/models/meal_prep_schedule_creator_spec.rb
@@ -6,26 +6,13 @@ RSpec.describe MealPrepSchedule::Creator, type: :model do
 
     it '作り置きスケジュール作成と同時に、アイテムも作成されること' do
       meal_prep_schedule_params = { name: 'スケジュール1'}
-      
-      schedule = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params).schedule
+      initial_count_params = { main_count: 4, side_count: 7 }
+
+      schedule = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params, initial_count_params).schedule
 
       expect(schedule.name).to eq 'スケジュール1'
       expect(schedule.items.where(meal_type: :main).size).to eq 4
       expect(schedule.items.where(meal_type: :side).size).to eq 7
-    end
-
-    describe '誤ったアイテムが作成された場合' do
-      let!(:invalid_params) { [ { meal_type: :main } ] }
-
-      it 'スケジュールとアイテムの両方が作成されないこと' do
-        stub_const("MealPrepSchedule::Creator::INITAL_ITEM_PARAMS", invalid_params)
-
-        result = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params)
-        expect(result.succeeded).to eq false
-        expect(result.schedule).to eq nil
-        expect(MealPrepSchedule.all.size).to eq 0
-        expect(MealPrepSchedule::Item.all.size).to eq 0
-      end
     end
   end
 end
