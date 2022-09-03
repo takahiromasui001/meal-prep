1d29dcaf9c4cfc1a1899f7f2d31412d8f3c73150
Takahiro Masui <takahiro.masui@luxiar.com>
2022-09-04 00:39:48 +0900

    #14 spec再修正

--- spec/models/meal_prep_schedule_creator_spec.rb
+++ spec/models/meal_prep_schedule_creator_spec.rb
@@ -21,38 +21,97 @@ RSpec.describe MealPrepSchedule::Creator, type: :model do
       let!(:past_item_side2) { create(:meal_prep_schedule_item, name: '引き継ぎサイド2', prepared: true, meal_type: :side, meal_prep_schedule_id: past_schedule.id) }
       let!(:past_item_side3) { create(:meal_prep_schedule_item, name: '引き継ぎ消費済サイド', prepared: true, meal_type: :side, remaining_rate: 0, meal_prep_schedule_id: past_schedule.id) }
 
-      it '作り置きスケジュール作成時に、以前のスケジュールの未消化のアイテムが引き継がれること' do
-        schedule = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params, initial_count_params, past_schedule.id).schedule
+      context '引き継ぎが2品でどちらも残量100%の場合' do
+        it '2品分は作成済と判断され、初期作成の作り置きアイテムが2品分減ること' do
+          schedule = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params, initial_count_params, past_schedule.id).schedule
 
-        expect(schedule.items.map(&:name).include?('引き継ぎメイン1')).to eq true
-        expect(schedule.items.map(&:name).include?('引き継ぎメイン2')).to eq true
-        expect(schedule.items.map(&:name).include?('引き継ぎサイド1')).to eq true
-        expect(schedule.items.map(&:name).include?('引き継ぎサイド2')).to eq true
-        expect(schedule.items.map(&:name).include?('引き継ぎ消費済サイド')).to eq false
-        expect(schedule.items.main.size).to eq 4
-        expect(schedule.items.side.size).to eq 7
+          item_names = schedule.items.map(&:name)
+          expect(item_names.include?('引き継ぎメイン1')).to eq true
+          expect(item_names.include?('引き継ぎメイン2')).to eq true
+          expect(item_names.include?('引き継ぎサイド1')).to eq true
+          expect(item_names.include?('引き継ぎサイド2')).to eq true
+          expect(item_names.include?('引き継ぎ消費済サイド')).to eq false
+          expect(item_names.include?('主菜2')).to eq true
+          expect(item_names.include?('主菜3')).to eq false
+          expect(item_names.include?('副菜5')).to eq true
+          expect(item_names.include?('副菜6')).to eq false
+          expect(schedule.items.main.size).to eq 4
+          expect(schedule.items.side.size).to eq 7
+        end
+      end
 
-        past_item_main1.update(remaining_rate: 30)
-        past_item_side1.update(remaining_rate: 30)
-        schedule2 = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params, initial_count_params, past_schedule.id).schedule
-        expect(schedule2.items.map(&:name).include?('引き継ぎメイン1')).to eq true
-        expect(schedule2.items.map(&:name).include?('引き継ぎメイン2')).to eq true
-        expect(schedule2.items.map(&:name).include?('引き継ぎサイド1')).to eq true
-        expect(schedule2.items.map(&:name).include?('引き継ぎサイド2')).to eq true
-        expect(schedule2.items.map(&:name).include?('引き継ぎ消費済サイド')).to eq false
-        expect(schedule2.items.main.size).to eq 5
-        expect(schedule2.items.side.size).to eq 8
+      context '引き継ぎが2品で片方が残量50%の場合' do
+        before do
+          past_item_main1.update(remaining_rate: 50)
+          past_item_side1.update(remaining_rate: 50)
+        end
 
-        past_item_main2.update(remaining_rate: 30)
-        past_item_side2.update(remaining_rate: 30)
-        schedule3 = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params, initial_count_params, past_schedule.id).schedule
-        expect(schedule3.items.map(&:name).include?('引き継ぎメイン1')).to eq true
-        expect(schedule3.items.map(&:name).include?('引き継ぎメイン2')).to eq true
-        expect(schedule3.items.map(&:name).include?('引き継ぎサイド1')).to eq true
-        expect(schedule3.items.map(&:name).include?('引き継ぎサイド2')).to eq true
-        expect(schedule3.items.map(&:name).include?('引き継ぎ消費済サイド')).to eq false
-        expect(schedule3.items.main.size).to eq 6
-        expect(schedule3.items.side.size).to eq 9
+        it '1品分だけ作成済と判断され、初期作成の作り置きアイテムが1品分減ること' do
+          schedule = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params, initial_count_params, past_schedule.id).schedule
+
+          item_names = schedule.items.map(&:name)
+          expect(item_names.include?('引き継ぎメイン1')).to eq true
+          expect(item_names.include?('引き継ぎメイン2')).to eq true
+          expect(item_names.include?('引き継ぎサイド1')).to eq true
+          expect(item_names.include?('引き継ぎサイド2')).to eq true
+          expect(item_names.include?('引き継ぎ消費済サイド')).to eq false
+          expect(item_names.include?('主菜3')).to eq true
+          expect(item_names.include?('主菜4')).to eq false
+          expect(item_names.include?('副菜6')).to eq true
+          expect(item_names.include?('副菜7')).to eq false
+          expect(schedule.items.main.size).to eq 5
+          expect(schedule.items.side.size).to eq 8
+        end
+      end
+
+      context '引き継ぎが2品で両方残量50%の場合' do
+        before do
+          past_item_main1.update(remaining_rate: 50)
+          past_item_main2.update(remaining_rate: 50)
+          past_item_side1.update(remaining_rate: 50)
+          past_item_side2.update(remaining_rate: 50)
+        end
+
+        it '1品分だけ作成済と判断され、初期作成の作り置きアイテムが1品分減ること' do
+          schedule = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params, initial_count_params, past_schedule.id).schedule
+
+          item_names = schedule.items.map(&:name)
+          expect(item_names.include?('引き継ぎメイン1')).to eq true
+          expect(item_names.include?('引き継ぎメイン2')).to eq true
+          expect(item_names.include?('引き継ぎサイド1')).to eq true
+          expect(item_names.include?('引き継ぎサイド2')).to eq true
+          expect(item_names.include?('引き継ぎ消費済サイド')).to eq false
+          expect(item_names.include?('主菜3')).to eq true
+          expect(item_names.include?('主菜4')).to eq false
+          expect(item_names.include?('副菜6')).to eq true
+          expect(item_names.include?('副菜7')).to eq false
+          expect(schedule.items.main.size).to eq 5
+          expect(schedule.items.side.size).to eq 8
+        end
+      end
+
+      context '引き継ぎが2品で両方残量30%の場合' do
+        before do
+          past_item_main1.update(remaining_rate: 30)
+          past_item_main2.update(remaining_rate: 30)
+          past_item_side1.update(remaining_rate: 30)
+          past_item_side2.update(remaining_rate: 30)
+        end
+
+        it '作成済の作り置きは無しと判断され、初期作成の作り置きアイテムの数は指定通りになること' do
+          schedule = MealPrepSchedule::Creator.new.create_schedule!(meal_prep_schedule_params, initial_count_params, past_schedule.id).schedule
+
+          item_names = schedule.items.map(&:name)
+          expect(item_names.include?('引き継ぎメイン1')).to eq true
+          expect(item_names.include?('引き継ぎメイン2')).to eq true
+          expect(item_names.include?('引き継ぎサイド1')).to eq true
+          expect(item_names.include?('引き継ぎサイド2')).to eq true
+          expect(item_names.include?('引き継ぎ消費済サイド')).to eq false
+          expect(item_names.include?('主菜4')).to eq true
+          expect(item_names.include?('副菜7')).to eq true
+          expect(schedule.items.main.size).to eq 6
+          expect(schedule.items.side.size).to eq 9
+        end
       end
     end
   end
