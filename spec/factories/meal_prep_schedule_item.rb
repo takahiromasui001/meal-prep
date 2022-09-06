c261c995ff54bfaec87c29817e36cc37e227bbf0
Takahiro Masui <takahiro.masui@luxiar.com>
2022-09-06 09:48:16 +0900

    #14 不要なコメントの削除

--- spec/factories/meal_prep_schedule_item.rb
+++ spec/factories/meal_prep_schedule_item.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
   factory :meal_prep_schedule_item, class: MealPrepSchedule::Item do
     name { "item" }
     meal_type { :main }
-
-    # association :schedule, factory: :meal_prep_schedule
   end
 end
