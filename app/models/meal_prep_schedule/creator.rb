df46d37062b59fbe8991f3c4873f4c5569e26735
Takahiro Masui <takahiro.masui@luxiar.com>
2022-09-06 09:36:52 +0900

    #14 リファクタリング

--- app/models/meal_prep_schedule/creator.rb
+++ app/models/meal_prep_schedule/creator.rb
@@ -13,16 +13,15 @@ class MealPrepSchedule::Creator
         duplicate_prepared_items(base_item_schedule_id, meal_prep_schedule.id)
       end
 
-      # meal pointから残りの要作成アイテムを計算(小数点切り捨て)
+      # 引き継ぎ対象アイテムのmeal pointから残りの要作成アイテムを計算(小数点切り捨て)
       sum_of_prepared_main_meal_point = meal_prep_schedule.items.main.prepared.map(&:meal_point).sum
       sum_of_created_side_meal_point = meal_prep_schedule.items.side.prepared.map(&:meal_point).sum
-
-      initial_count_params_2 = {
-        main_count: initial_item_count[:main] - sum_of_prepared_main_meal_point.floor,
-        side_count: initial_item_count[:side] - sum_of_created_side_meal_point.floor
+      initial_count_params = {
+        main: initial_item_count[:main] - sum_of_prepared_main_meal_point.floor,
+        side: initial_item_count[:side] - sum_of_created_side_meal_point.floor
       }
 
-      create_initial_item_params(initial_count_params_2).each do |item_param|
+      create_initial_item_params(initial_count_params).each do |item_param|
         meal_prep_schedule.items.create!(item_param)
       end
     end
@@ -35,9 +34,8 @@ class MealPrepSchedule::Creator
   private
 
   def create_initial_item_params(initial_count_params)
-    initial_count_hash = initial_count_params.to_h.map {|k, v| [k.to_sym, v.to_i]}.to_h
-    main_count = initial_count_hash[:main_count]
-    side_count = initial_count_hash[:side_count]
+    main_count = initial_count_params[:main]
+    side_count = initial_count_params[:side]
 
     main_params = (1..main_count).map { |i| { name: "主菜#{i}", meal_type: :main } }
     side_params = (1..side_count).map { |i| { name: "副菜#{i}", meal_type: :side } }
